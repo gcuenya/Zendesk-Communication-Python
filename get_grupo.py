4492e0ca897228b99ef9aa2ed708eabfec0aadd6
Guillermo Adrian Cuenya <42759901+gcuenya@users.noreply.github.com>
2018-09-12 02:04:07 -0300

    Create get_grupo.py

---
+++ get_grupo.py
@@ -0,0 +1,25 @@
+import requests
+
+# Set the request parameters
+url = 'https://ignetworks.zendesk.com/agent/dashboard/api/v2/groups.json'
+user = 'gcuenya@ignetworks.com'
+pwd = '*****'
+
+# Do the HTTP get request
+response = requests.get(url, auth=(user, pwd))
+
+# Check for HTTP codes other than 200
+if response.status_code != 200:
+    print('Status:', response.status_code, 'Problem with the request. Exiting.')
+    exit()
+
+# Decode the JSON response into a dictionary and use the data
+data = response.json()
+
+# Example 1: Print the name of the first group in the list
+print( 'First group = ', data['groups'][0]['name'] )
+
+# Example 2: Print the name of each group in the list
+group_list = data['groups']
+for group in group_list:
+    print(group['name'])
